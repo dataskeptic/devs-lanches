cc7a1b571e9ac6288f3db646328107df41730c44
João Gabriel <joaogabrielsg.00@gmail.com>
2021-06-01 10:14:20 -0300

    :hamburger: add snack example page

--- src/pages/Menu/Burgers/index.tsx
+++ src/pages/Menu/Burgers/index.tsx
@@ -1,22 +1,42 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { FiPlus } from 'react-icons/fi';
 
 import { api } from '../../../services/api';
+import { SnackData } from '../../../interface/snackData';
+import { currencyFormat } from '../../../helpers/currencyFormat';
 
 import { SnackTitle } from '../../../components/SnackTitle';
 
 export function Burgers() {
-	useEffect(() => {
-		async function getBurgers() {
-			const { data } = await api.get('/burgers');
-			console.log(data);
-		}
+	const [burgers, setBurgers] = useState<SnackData[]>([]);
 
-		getBurgers();
+	useEffect(() => {
+		(async () => {
+			const { data } = await api.get('/burgers');
+			setBurgers(data);
+		})();
 	}, []);
 
 	return (
 		<>
 			<SnackTitle>Hambúrgueres</SnackTitle>
+			{burgers.length !== 0 && (
+				<div className="snacks">
+					{burgers.map((burger) => (
+						<div key={burger.id} className="snack">
+							<h2>{burger.name}</h2>
+							<img src={burger.image} alt={burger.name} />
+							<p>{burger.description}</p>
+							<div>
+								<strong>{currencyFormat(burger.price)}</strong>
+								<button type="button">
+									<FiPlus />
+								</button>
+							</div>
+						</div>
+					))}
+				</div>
+			)}
 		</>
 	);
 }
