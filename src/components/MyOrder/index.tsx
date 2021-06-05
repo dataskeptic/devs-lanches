d49b80c661f02800690b33d45a0fa02f8bd44b57
João Gabriel <joaogabrielsg.00@gmail.com>
2021-06-05 11:34:31 -0300

    :shoppint_cart: update my order component

--- src/components/MyOrder/index.tsx
+++ src/components/MyOrder/index.tsx
@@ -1,12 +1,16 @@
 import { ReactComponent as CartIcon } from '../../assets/shopping-cart.svg';
+import { useOrder } from '../../contexts/OrderContext';
 
 import { Container } from './styles';
 
 export function MyOrder() {
+	const { cart } = useOrder();
+
 	return (
 		<Container to="cart">
 			<span>Meu Pedido</span>
 			<CartIcon />
+			{cart.length !== 0 && <span>{`${cart.length}`.padStart(2, '0')}</span>}
 		</Container>
 	);
 }
