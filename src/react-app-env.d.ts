8203ea4a7a6c19fb9c88d6b86fc19232a137c03a
João Gabriel <joaogabrielsg.00@gmail.com>
2021-06-11 22:59:22 -0300

    :sparkles: add default theme interface

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="react-scripts" />
+
+import 'styled-components';
+
+declare module 'styled-components' {
+	export interface DefaultTheme {
+		colors: {
+			background: string;
+			red: string;
+			yellow: string;
+			white: string;
+			black: string;
+			blackLight: string;
+		};
+	}
+}
